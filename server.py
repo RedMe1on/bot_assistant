9a685b5e9806e6d3b5a60e42e71b0d86a2db33eb
RedMe1on <42094120+RedMe1on@users.noreply.github.com>
2021-03-24 02:31:40 +0300

    fix bug with date add new opportunity with add task with date without year add data tasks (today, tomorrow, week) add handler with message

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher, executor, types
@@ -41,6 +41,7 @@ async def del_tasks(message: types.Message):
 
 
 @dp.message_handler(state='*', commands=['reset_state'])
+@dp.message_handler(lambda message: 'назад' in message.text.lower() or 'отмен' in message.text.lower())
 async def reset_state(message: types.Message, state: FSMContext):
     current_state = await state.get_state()
     if current_state is None:
@@ -52,6 +53,7 @@ async def reset_state(message: types.Message, state: FSMContext):
 
 
 @dp.message_handler(commands=['all_task'])
+@dp.message_handler(lambda message: 'все' in message.text.lower() or 'спис' in message.text.lower())
 async def send_list_tasks(message: types.Message):
     """Выводит список задач"""
     tasks = Tasks().get_all_tasks()
@@ -67,6 +69,7 @@ async def send_list_tasks(message: types.Message):
 
 
 @dp.message_handler(commands=['roulette'])
+@dp.message_handler(lambda message: 'рулет' in message.text.lower())
 async def send_random_tasks(message: types.Message):
     """Выводит рандомную задачу"""
     random_task = Tasks().get_random_task()
@@ -77,17 +80,54 @@ async def send_random_tasks(message: types.Message):
 
 
 @dp.message_handler(commands=['today_task'])
+@dp.message_handler(lambda message: 'сегод' in message.text.lower())
 async def send_today_tasks(message: types.Message):
     """Выводит задачи на сегодня"""
-    today_task = Tasks().get_today_tasks()
-    result_string = 'Сегодня у тебя много работы, прям завал:'
-    for index, task in enumerate(today_task):
-        result_string += f'\n{index + 1}. {task.get("description")}' \
-                         f'\nУдалить: /del{task.get("id")}'
+    today_task = Tasks().get_tasks_with_days_interval(0)
+    if len(today_task) != 0:
+        result_string = 'Сегодня у тебя много работы, прям завал:'
+        for index, task in enumerate(today_task):
+            result_string += f'\n{index + 1}. {task.get("description")}' \
+                             f'\nУдалить: /del{task.get("id")}'
+    else:
+        result_string = 'Ты сегодня самый занятой человек планеты, у тебя целых 0 задач на сегодня!\n' \
+                        'Лучше попроси парочку из полного списка или добавь себе работы!'
+    await message.answer(result_string)
+
+
+@dp.message_handler(commands=['tomorrow_task'])
+@dp.message_handler(lambda message: 'завт' in message.text.lower())
+async def send_tomorrow_tasks(message: types.Message):
+    """Выводит задачи на сегодня"""
+    today_task = Tasks().get_tasks_with_days_interval(1)
+    if len(today_task) != 0:
+        result_string = 'Завтра у тебя много работы, прям завал:'
+        for index, task in enumerate(today_task):
+            result_string += f'\n{index + 1}. {task.get("description")}' \
+                             f'\nУдалить: /del{task.get("id")}'
+    else:
+        result_string = 'Придумай, что делать завтра, потому что список задач на завтра ПУСТ! (как и мой интеллект)'
+    await message.answer(result_string)
+
+
+@dp.message_handler(commands=['week_task'])
+@dp.message_handler(lambda message: 'недел' in message.text.lower())
+async def send_list_tasks_during_the_week(message: types.Message):
+    """Выводит задачи на сегодня"""
+    today_task = Tasks().get_tasks_with_days_interval(1)
+    if len(today_task) != 0:
+        result_string = 'Задачи на неделю вперед ждут, пока ты их сделаешь когда-нибудь потом. На, посмотри на них:'
+        for index, task in enumerate(today_task):
+            result_string += f'\n{index + 1}. {task.get("description")} \nДата: {task.get("date_")} ' \
+                             f'\nУдалить: /del{task.get("id")}'
+    else:
+        result_string = 'Неделя обещает быть максимально удачной! У тебя нет задач, их 0. \n' \
+                        'Покрути рулетку или добавь себе работы!'
     await message.answer(result_string)
 
 
 @dp.message_handler(state='*', commands=['add_task'])
+@dp.message_handler(lambda message: 'добав' in message.text.lower())
 async def message_for_add_task(message: types.Message):
     """Просит добавить новую задачу"""
 
@@ -106,6 +146,9 @@ async def add_task(message: types.Message, state: FSMContext):
     except exceptions.NotCorrectMessage as e:
         await message.answer(str(e))
         return
+    except exceptions.NotCorrectData as e:
+        await message.answer(str(e))
+        return
     if task.date_:
         answer_message = (
             f"Задача успешно добавлена.\n"
@@ -119,5 +162,31 @@ async def add_task(message: types.Message, state: FSMContext):
     await state.finish()
 
 
+
+
+
+# async def my_func():
+#     # твоя логика с отправкой сообщений тут
+#     print('hi, father')
+#     when_to_call = loop.time() + delay  # delay -- промежуток времени в секундах.
+#     loop.call_at(when_to_call, my_callback)
+#
+#
+# def my_callback():
+#     asyncio.create_task(my_func())
+#
+#
+# # async def gg():
+# #     while True:
+# #         await bot.send_message(567115076, 'test')
+# #         await asyncio.sleep(10)
+
+
 if __name__ == '__main__':
+    # loop = asyncio.get_event_loop()
+    # delay = 10.0
+    # loop.run_until_complete(my_callback())
     executor.start_polling(dp, skip_updates=True)
+    # loop.run_forever()
+
+
--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@
 import random
 import re
 from typing import Dict, List, NamedTuple, Any
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 import db
 import exceptions
 
@@ -38,13 +38,22 @@ class Tasks:
         """Возвращает случайную задачу"""
         return random.choice(self._tasks)
 
-    def get_today_tasks(self) -> List[dict]:
-        """Возвращает задачи на сегодня"""
-        today_tasks = []
+    def get_tasks_with_days_interval(self, number_of_days: int) -> List[dict]:
+        """Возвращает задачи за определенный период"""
+        data_tasks = []
+        date_task = datetime.now() + timedelta(days=number_of_days)
         for task in self._tasks:
-            if task.date_ == datetime.now().strftime("%d/%m/%y"):
-                today_tasks.append(task)
-        return today_tasks
+            if task.get('date_') == date_task.strftime("%d/%m/%y"):
+                data_tasks.append(task)
+        return data_tasks
+
+    def get_overdue_tasks(self) -> List[dict]:
+        """Возвращает просроченные задачи"""
+        data_tasks = []
+        for task in self._tasks:
+            if task.get('date_') < datetime.now().strftime("%d/%m/%y"):
+                data_tasks.append(task)
+        return data_tasks
 
     @staticmethod
     def delete_task(row_id: int):
@@ -64,27 +73,46 @@ class Tasks:
     @staticmethod
     def _parse_message(raw_message: str) -> Message:
         """Парсит текст пришедшего сообщения о новой задаче."""
-        if re.match(r"([\d ]+)", raw_message.strip()):
-            regexp_result = re.match(r"(\d{,2}[./-]\d{,2}[./-]\d{2,4}) (.*)", raw_message.strip())
-            if not regexp_result or not regexp_result.group(0) \
+        regexp_result = re.match(r"(\d{,2}[./-]\d{,2}[./-]?\d{0,4}) (.*)", raw_message.strip())
+        if not regexp_result:
+            task_date = None
+            task_text = raw_message.strip()
+        else:
+            if not regexp_result.group(0) \
                     or not regexp_result.group(1) or not regexp_result.group(2):
                 raise exceptions.NotCorrectMessage(
-                    "Не могу понять сообщение. "
-                    "Пиши в нужном формате без даты или с ней "
-                    "(дату в формате dd.mm.yy - можно использовать разделители, но только один тип: './-'), "
+                    "Не могу понять сообщение.\n "
+                    "Пиши в нужном формате без даты или с ней. \n"
+                    "Дату в формате dd.mm.yy - можно использовать разделители, но только один тип: './-', "
                     "например:\n12.12.2020 Классная задача по гачимучи"
                     "\nКлассная задача по гачимучи"
                     "\n12.2.21 Классная задача по гачимучи")
             task_date = Tasks._format_date_with_validate(regexp_result.group(1))
             task_text = regexp_result.group(2).strip()
-        else:
-            task_date = None
-            task_text = raw_message.strip()
         return Message(date=task_date, description=task_text)
 
     @staticmethod
     def _format_date_with_validate(date_task: str) -> str:
         """Приведение даты к общему формату для записи в БД"""
+        split_date = Tasks.split_date(date_task)
+        try:
+            day, mouth, year = split_date[0], split_date[1], split_date[2]
+        except IndexError:
+            day, mouth = split_date[0], split_date[1]
+            year = str(datetime.now().year)
+        try:
+            if len(year) == 2:
+                date_task = datetime.strptime(f"{int(day)}/{int(mouth)}/{int(year)}", "%d/%m/%y").strftime("%d/%m/%y")
+            else:
+                date_task = date(int(year), int(mouth), int(day)).strftime("%d/%m/%y")
+        except ValueError:
+            raise exceptions.NotCorrectData('Некоректная дата. Укажите существующую дату.\n'
+                                            'Месяцев может быть только 12, а дней столько, сколько в указаном месяце')
+        return date_task
+
+    @staticmethod
+    def split_date(date_task):
+        """Разделяет дату по одному из трех доступных разделителей"""
         if len(date_task.split('/')) != 1:
             split_date = date_task.split('/')
         elif len(date_task.split('.')) != 1:
@@ -94,14 +122,4 @@ class Tasks:
         else:
             raise exceptions.NotCorrectData('Некоректная дата. Введите дату в формате dd.mm.yy\n'
                                             'Можно использовать разделители, но только один тип: "./-" ')
-        day, mouth, year = split_date[0], split_date[1], split_date[2]
-
-        try:
-            if len(year) == 2:
-                date_task = datetime.strptime(f"{int(day)}/{int(mouth)}/{int(year)}", "%d/%m/%y").strftime("%d/%m/%y")
-            else:
-                date_task = date(int(year), int(mouth), int(day)).strftime("%d/%m/%y")
-        except ValueError:
-            raise exceptions.NotCorrectData('Некоректная дата. Укажите существующую дату\n'
-                                            'Месяцев может быть только 12, а дней столько, сколько в указаном месяце')
-        return date_task
+        return split_date
