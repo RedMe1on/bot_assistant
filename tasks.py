1c1ba3ddb4ced08e931ff51ee40fe81a95874ed7
RedMe1on <42094120+RedMe1on@users.noreply.github.com>
2021-04-07 00:44:42 +0300

    fix bug with overdue tasks

--- tasks.py
+++ tasks.py
@@ -36,7 +36,9 @@ class Tasks(ObjectMixin):
         """Возвращает просроченные задачи"""
         data_tasks = []
         for task in self._all_objects:
-            if task.get('date_') < datetime.now().strftime("%d/%m/%y"):
+            date_list = task.get('date_').split('/')
+            date_ = datetime(int(date_list[2]), int(date_list[1]), int(date_list[0]))
+            if date_ < datetime.now():
                 data_tasks.append(task)
         return data_tasks
 
